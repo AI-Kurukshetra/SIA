7f654199e4878faaa6a95a2f837d41fa21102449
Kishan Prajapati <prajapatikishan.kp2002@gmail.com>
2026-03-14 12:03:24 +0530

    changed

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,6 +2,9 @@ import type { ReactNode } from "react";
 
 import { DashboardShell } from "@/components/dashboard-shell";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function DashboardLayout({
   children
 }: {
